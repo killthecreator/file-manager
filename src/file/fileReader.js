3800fee9903e92eea89e0c3aa698e44b7d1ae5bc
Gleb <podiumfffuuu@gmail.com>
2022-12-04 11:11:08 +0300

    fix: fix cat function to catch error when file does not exist

--- src/file/fileReader.js
+++ src/file/fileReader.js
@@ -1,9 +1,11 @@
 import {createReadStream} from 'fs';
+import { readFile as read } from 'fs/promises';
 import { join } from 'path';
 
 export const readFile = async (curDir, fileName) => {
+    await read(join(curDir, fileName)); /* Check for file existence/accessibility */
     return new Promise((res) => {
-        const stream = createReadStream(join(curDir, fileName), 'utf8')
+        const stream =  createReadStream(join(curDir, fileName), 'utf8')
             .on('data', chunck => console.log(chunck))
             .on('end', () => res());
     });
