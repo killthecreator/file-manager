419f7164b8b45b2d53bc726c528330f12acd38f0
Gleb <podiumfffuuu@gmail.com>
2022-12-07 13:15:14 +0300

    feat: add file existense check for rn

--- src/file/fileRenamer.js
+++ src/file/fileRenamer.js
@@ -1,6 +1,12 @@
 import {rename} from 'fs/promises';
 import {join, dirname} from 'path';
+import { access } from 'fs/promises';
 
 export const renameFile = async (curDir, fileName, newFileName) => {
-    await rename(join(curDir, fileName), join(curDir, dirname(fileName), newFileName));
+    try {
+        await (access(join(curDir, dirname(fileName), newFileName)));
+        return Promise.reject();
+    } catch {
+        await rename(join(curDir, fileName), join(curDir, dirname(fileName), newFileName));
+    }
 };
